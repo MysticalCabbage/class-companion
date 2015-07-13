3ede9492347a4714421df6098768b5a16e58b54e
David Hom <dlhom3@gmail.com>
2015-07-12 23:19:53 -0700

    (refactor) refactor node server
    modularized node express server

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var app = require('./server/server.js');
 
-var app = express();
-
-app.set('port', (process.env.PORT || 3000));
-
-app.use('/', express.static(path.join(__dirname, '/dist')));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.set('port', process.env.PORT);
 
 app.listen(app.get('port'), function() {
   console.log('Listening on port: ' + app.get('port'));
