6a0b65f4c7c1dcab48a2aa78443e813284b0a984
David Hom <dlhom3@gmail.com>
2015-07-15 09:42:55 -0700

    (feat) fix app component to reflect new signup and login route

--- src/js/app.js
+++ src/js/app.js
@@ -5,20 +5,23 @@ var TeacherClass = require('./components/TeacherClass.react');
 var TeacherForm = require('./components/TeacherForm.react');
 var ClassroomDashboard = require('./components/ClassroomDashboard');
 var Auth = require('./components/Auth.react');
+var Signup = require('./components/Signup.react');
+var Login = require('./components/Login.react');
 
 var Router = require('react-router');
 var Route = Router.Route;
 var RouteHandler = Router.RouteHandler;
 var DefaultRoute = Router.DefaultRoute;
+var Redirect = Router.Redirect;
 var Link = Router.Link;
 var Navigation = Router.Navigation;
 
 var routes = (
-  <Route path="/" handler={App}>
+  <Route name="root" path="/" handler={App}>
   	<DefaultRoute handler={TeacherDashboard}/>
-    <Route path="login" handler={Auth.Login}/>
-    <Route path="logout" handler={Auth.Logout}/>
-    <Route path="signup" handler={Auth.Signup}/>
+    <Route path="login" handler={Login}/>
+    <Redirect from="logout" to="root" />
+    <Route path="signup" handler={Signup}/>
     <Route path="teacherDashboard" handler={TeacherDashboard}/>
       <Route path="teacherClass/:id" handler={TeacherClass}/>
       <Route path="teacherForm" handler={TeacherForm}/>
