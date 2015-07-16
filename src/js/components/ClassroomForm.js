e6c8d74836b4cb2e1f621fee90650286a0f96647
Stacy Huang <syhhuang88@gmail.com>
2015-07-15 19:43:10 -0700

    (Refactor) Refactor ClassroomForm

---
+++ src/js/components/ClassroomForm.js
@@ -0,0 +1,40 @@
+var React = require('react');
+var ClassroomActions = require('../actions/ClassroomActions');
+var ClassroomForm = React.createClass({
+  getInitialState: function(){
+    return {
+      newStudent: ''
+    }
+  },
+  handleAddStudent: function(){
+    
+    ClassroomActions.addStudent(this.state.newStudent);
+    var form = document.getElementById('newStudent');
+    form.value = '';
+  },
+  updateStudent: function(e){
+    this.setState({
+      newStudent: e.target.value
+    });
+  },
+  render: function() {
+    return (
+      <div className="classroomForm container">
+        <div className="row">
+          <div className="col-sm-6 well text-center">
+            <form>
+              <label for="">Add student</label>
+              <div className="form-group">
+                <input type="text" id="newStudent" onChange={this.updateStudent} className="form-control" placeholder="new student"  />
+              </div>
+              <button type="submit" onClick={this.handleAddStudent} id="addNewStudent" className="btn btn-primary btn-block submit-button">Add student!</button>
+            </form>
+          </div> 
+        </div> 
+      </div>
+    );
+  }
+
+});
+
+module.exports = ClassroomForm;
