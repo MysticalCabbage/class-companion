58965ebc2f4eaa6ee23270ac0f4b7af8eb329bfb
David Hom <dlhom3@gmail.com>
2015-07-15 23:12:59 -0700

    (feat) add FirebaseStore and AuthStore to TeachDashboard

--- src/js/components/TeacherDashboard.react.js
+++ src/js/components/TeacherDashboard.react.js
@@ -3,10 +3,14 @@ var TeacherClass = require('./TeacherClass.react');
 var TeacherActions = require('../actions/TeacherActions');
 var TeacherStore = require('../stores/TeacherStore');
 var TeacherForm = require('./TeacherForm.react');
+var AuthStore = require('../stores/AuthStore');
+var FirebaseStore = require('../stores/FirebaseStore');
 var Router = require('react-router');
 var Link = Router.Link;
 var _ = require('underscore');
 var authStore = require('../stores/AuthStore');
+
+var firebaseRef = FirebaseStore.getDb();
 
 var TeacherDashboard = React.createClass({
   // Invoke TeacherStore.getList() and set the result to the list property on our state
