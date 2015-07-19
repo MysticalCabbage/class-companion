1d7245a2241f6de9af4b59ac023edb8821b27640
David Hom <dlhom3@gmail.com>
2015-07-19 09:25:59 -0700

    (feat) add StudentGroup component

--- src/js/components/StudentGroup.react.js
+++ src/js/components/StudentGroup.react.js
@@ -1,10 +1,41 @@
 var React = require('react');
+var StudentSelectionStore = require('../stores/StudentSelectionStore');
+var _ = require('underscore');
 
 var StudentGroup = React.createClass({
+  getInitialState: function(){
+    return {
+      groups: StudentSelectionStore.getGroup()
+    }
+  },
+
+  componentDidMount: function(){ 
+    StudentSelectionStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount: function(){
+    StudentSelectionStore.removeChangeListener(this._onChange);
+  },
+
+  _onChange: function(){
+    this.setState({
+      groups: StudentSelectionStore.getGroup()
+    });
+  },
   closeModal: function(){
     console.log('close');
   },
   render: function(){
+    var groupStudent;
+    var groups = _.map(this.state.groups, function(group, index){
+      groupStudent = _.reduce(group, function(acc, cur){
+        return acc += cur.studentTitle + ' ';
+      },' ');
+      return (
+        <p key={index}> {groupStudent}</p>
+        )
+    });
+
     return (
       <div className="StudentRandom col-md-3">
         <div className="well">
@@ -14,7 +45,7 @@ var StudentGroup = React.createClass({
             <button type="button" className="close" aria-label="Close" onClick={this.closeModal}><span aria-hidden="true">&times;</span></button>
           </div>
         </div>
-          <div>GROUPS</div>
+          <div>{groups}</div>
         </div>
       </div>
     );
