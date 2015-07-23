f1796b3a98ff70a8c35a2cc0ef3feb8ea2c654a3
David Hom <dlhom3@gmail.com>
2015-07-23 14:25:52 -0700

    (feat) add ClassroomStore unit test spec

--- specs/classroomstore-test.js
+++ specs/classroomstore-test.js
@@ -14,4 +14,24 @@ describe("ClassroomStore", function() {
     expect(ClassroomStore).to.be.an('object');
   });
 
+  it('should have a addChangeListener method', function(){
+    expect(ClassroomStore.addChangeListener).to.exist;
+  });
+  
+  it('should have a removeChangeListener method', function(){
+    expect(ClassroomStore.removeChangeListener).to.exist;
+  });
+
+  it('should have a getList method', function(){
+    expect(ClassroomStore.getList).to.exist;
+  });
+
+  it('should have a getInfo method', function(){
+    expect(ClassroomStore.getInfo).to.exist;
+  });
+  
+  it('should have a getAttendance method', function(){
+    expect(ClassroomStore.getAttendance).to.exist;
+  });
+
 });
