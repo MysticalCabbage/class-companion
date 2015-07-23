4293e498e6570e66ede371f9db8bde0b91fdaa45
David Hom <dlhom3@gmail.com>
2015-07-23 15:00:39 -0700

    (feat) add StudentselectionStore unit test spec

--- specs/studentselectionstore-test.js
+++ specs/studentselectionstore-test.js
@@ -13,5 +13,28 @@ describe("StudentSelectionStore", function() {
   it("should be an object", function() {
     expect(StudentSelectionStore).to.be.an('object');
   });
+
+  it('should have a addChangeListener method', function(){
+    expect(StudentSelectionStore.addChangeListener).to.exist;
+  });
   
+  it('should have a removeChangeListener method', function(){
+    expect(StudentSelectionStore.removeChangeListener).to.exist;
+  });
+
+  it('should have a getRandom method', function(){
+    expect(StudentSelectionStore.getRandom).to.exist;
+  });
+
+  it('should have a getGroup method', function(){
+    expect(StudentSelectionStore.getGroup).to.exist;
+  });
+
+  it('getRandom method should return a string', function(){
+    expect(StudentSelectionStore.getRandom()).to.equal('');
+  });
+  
+  it('getGroup method should return an array', function(){
+    expect(StudentSelectionStore.getGroup()).to.be.an('array');
+  });  
 });
