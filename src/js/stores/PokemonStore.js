069170350095fa12f457b667852b4d9289799bda
JD <jdstep23@gmail.com>
2015-07-25 09:33:51 -0700

    pokemon ajax requests properly throw errors

--- src/js/stores/PokemonStore.js
+++
@@ -1,9 +0,0 @@
-var AppDispatcher = require('../dispatcher/AppDispatcher');
-var ClassroomConstants = require('../constants/ClassroomConstants');
-var objectAssign = require('object-assign');
-var EventEmitter = require('events').EventEmitter;
-var FirebaseStore = require('./FirebaseStore');
-
-var CHANGE_EVENT = 'change';
-
-var firebaseRef = FirebaseStore.getDb();
