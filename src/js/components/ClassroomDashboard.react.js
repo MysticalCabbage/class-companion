08d48a426b370125235db2b42356740fcff92484
David Hom <dlhom3@gmail.com>
2015-07-28 15:23:11 -0700

    (fix) fix random modal timeout by reducing time

--- src/js/components/ClassroomDashboard.react.js
+++ src/js/components/ClassroomDashboard.react.js
@@ -88,12 +88,12 @@ var ClassroomDashboard = React.createClass({
       clearTimeout(randomModalTimer);
       randomModalTimer = setTimeout(function(){
         this.closeRandomModal();
-      }.bind(this), 5000);
+      }.bind(this), 3000);
     } else {
       this.setState({randomModal: true});
       randomModalTimer = setTimeout(function(){
         this.closeRandomModal();
-      }.bind(this), 5000);
+      }.bind(this), 3000);
     }
     
   },
