4d5829790e35e00a79ea6dfebe0d0ea799f11c9b
David Hom <dlhom3@gmail.com>
2015-07-27 19:35:24 -0700

    (feat) add style to group modal

--- src/js/components/StudentGroupForm.react.js
+++ src/js/components/StudentGroupForm.react.js
@@ -24,7 +24,7 @@ var StudentGroupForm = React.createClass({
     ClassroomActions.randGroup(groupNum);
     this.props.closeModal();
   },
-  
+
   render: function() {
     return (
       <div className="panel panel-info StudentGroupForm">
@@ -32,13 +32,16 @@ var StudentGroupForm = React.createClass({
           <button type="button" className="close" aria-label="Close" onClick={this.props.closeModal}><span aria-hidden="true">&times;</span></button>
           <h3 className="panel-title">Groups</h3>
         </div>
+
         <form className="form-horizontal" id="frmGroup" role="form" onSubmit={this.handleSubmit}>
+        <div className="well text-center">
           <div className="panel-body">
             <input pattern="[0-9]*" className="form-control" placeholder="Enter # of Groups" ref="groupNum" required/>
           </div>
           <button type="submit" id="selectGroups" className="btn btn-primary btn-block submit-button">Group!</button>
-        </form>
           <button type="button" id="removeGroups" className="btn btn-primary btn-block" onClick={this.handleSubmit}>Remove Groups</button>
+          </div>
+        </form>
       </div>
     );
   }
