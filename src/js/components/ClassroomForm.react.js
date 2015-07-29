90de4e0cb9d914d52e4a8353a8706916b63e5e39
David Hom <dlhom3@gmail.com>
2015-07-29 16:54:42 -0700

    (feat) add temporary ClassroomForm validation for adding students

--- src/js/components/ClassroomForm.react.js
+++ src/js/components/ClassroomForm.react.js
@@ -2,11 +2,13 @@ var React = require('react');
 var AuthStore = require('../stores/AuthStore');
 var ClassroomStore = require('../stores/ClassroomStore');
 var ClassroomActions = require('../actions/ClassroomActions');
+var _ = require('underscore');
 
 var ClassroomForm = React.createClass({
   getInitialState: function(){
     return {
-      newStudent: ''
+      newStudent: '',
+      errMsg: null
     }
   },
 
@@ -19,16 +21,41 @@ var ClassroomForm = React.createClass({
     }
   },
 
-
   handleAddStudent: function(e){
     e.preventDefault();
+
+    this.setState({errMsg: null });
+
     var newStudent = React.findDOMNode(this.refs.newStudent).value;
+
+/*  // validate user input  
+    var setErrMsg = function(errMsg){
+      this.setState({errMsg: errMsg});
+      React.findDOMNode(this.refs.newStudent).value = '';
+      return null;
+    }.bind(this);
+
+    var validateSpaces = newStudent.match(/\s/g);
+    var validateLength = _.reduce(newStudent.split(' '), function(max, cur){
+      return cur.length > max ? cur.length : max;
+    }, 0);
+
+    if(validateSpaces && validateSpaces.length > 2){
+      return setErrMsg('Student name should follow format.');
+    } else if(validateLength > 10){
+      return setErrMsg('Student first, middle, or last name should be 10 characters or less');
+    }*/
+
     var setBehavior = ClassroomStore.getInfo().behavior;
+
     for(var key in setBehavior){
       setBehavior[key] = 0;
     }
+
     ClassroomActions.addStudent({studentTitle: newStudent, behavior: setBehavior, behaviorTotal: 0  });
+
     React.findDOMNode(this.refs.newStudent).value = '';
+
     this.props.closeAddStudentModal();
   },
 
@@ -39,8 +66,9 @@ var ClassroomForm = React.createClass({
           <button type="button" className="close" aria-label="Close" onClick={this.props.closeAddStudentModal}><span aria-hidden="true">&times;</span></button>
           <form onSubmit={this.handleAddStudent}> 
             <label>Add student</label>
+            {this.state.errMsg ? <div className="errMsg">{this.state.errMsg}</div> : null}
             <div className="form-group">
-              <input type="text" ref="newStudent" id="newStudent" className="form-control" placeholder="Example: Pikachu" required />
+              <input type="text" ref="newStudent" id="newStudent" className="form-control" placeholder="Format: First Name Middle Initial Last Name" required />
             </div>
             <button type="submit" id="addNewStudent" className="btn btn-primary btn-block submit-button">Add student!</button>
           </form>
