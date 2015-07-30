a8761d95205bc01ce72a55fc31f638ac62e8d47f
Eric Kao <ericskao@gmail.com>
2015-07-29 20:36:08 -0700

    (feat) modal working for email form

--- src/js/components/EmailList.react.js
+++ src/js/components/EmailList.react.js
@@ -1,26 +1,82 @@
 var React = require('react');
+var Modal = require('react-modal');
 var HomeworkActions = require('../actions/HomeworkActions');
 var HomeworkStore = require('../stores/HomeworkStore');
 var ClassroomStore = require('../stores/ClassroomStore');
 var ClassroomActions = require('../actions/ClassroomActions');
+var AuthStore = require('../stores/AuthStore');
 var _ = require('underscore');
+
+
+var EmailForm = React.createClass({
+  getInitialState: function(){
+    return {
+      newEmail: ''
+    }
+  },
+  componentWillMount: function(){
+    if(!AuthStore.checkAuth()){
+      this.render = function () {
+        return false;
+      }
+      location.hash = '/';
+    }
+  },
+   handleAddEmail: function(e){
+    e.preventDefault();
+    var newEmail = React.findDOMNode(this.refs.newEmail).value;
+    // ClassroomActions.addEmail({studentTitle: newStudent, behavior: setBehavior, behaviorTotal: 0  });
+    React.findDOMNode(this.refs.newEmail).value = '';
+    this.props.closeEmailModal();
+  },
+  render: function() {
+    return (
+      <div className="emailForm">
+        <div className="well text-center">
+          <button type="button" className="close" aria-label="Close" onClick={this.props.closeEmailModal}><span aria-hidden="true">&times;</span></button>
+          <form onSubmit={this.handleAddEmail}> 
+            <label>Add E-Mail</label>
+            <div className="form-group">
+              <input type="text" ref="newEmail" id="newEmail" className="form-control" placeholder="Example: johndoe@example.com" required />
+            </div>
+            <button type="submit" id="addNewEmail" className="btn btn-primary btn-block submit-button">Add email!</button>
+          </form>
+        </div> 
+      </div>
+    );
+  }
+});
+
 
 var Student = React.createClass({
   getInitialState: function(){
     return {
       email: undefined,
-      parentEmail: undefined 
+      parentEmail: undefined,
+      emailModal: false
     }
   },
+
   clicked: function(){
     console.log("clicked");
   },  
+
   addStudent: function(){
     console.log("addstudent clicked");
   },
+
   addParent: function(){
     console.log("add parent clicked");
   },
+
+  openModal: function(){
+    this.setState({emailModal: true});
+  },
+
+  closeModal: function() {
+    this.setState({emailModal: false});
+  },
+
   render: function(){
     if(this.props.email === undefined){
       var email = "No email added! Click here to add";
@@ -33,13 +89,19 @@ var Student = React.createClass({
       var parentEmail = this.props.parentEmail;
     }
     return (
+        
         <tr>
-        <td>{this.props.studentTitle}</td>
-        <td><a onClick={this.addStudent}>{email}</a></td>
+        <td>{this.props.studentTitle}<Modal className="emailModal" 
+            isOpen={this.state.emailModal} 
+            onRequestClose={this.closeModal}>
+            <EmailForm closeEmailModal={this.closeModal}/>
+          </Modal></td>
+        <td><a onClick={this.openModal}>{email}</a></td>
         <td><a className='fa fa-envelope-o' onClick={this.clicked}></a></td>
         <td><a onClick={this.addParent}>{parentEmail}</a></td>
         <td><a className='fa fa-envelope-o' onClick={this.clicked}></a></td>
-        </tr>
+        </tr>    
+
     );
   }
 });
@@ -90,7 +152,7 @@ var EmailList = React.createClass({
             <tbody>
             {students}
             </tbody>
-          </table>    
+          </table>
       </div>
     );
   }
