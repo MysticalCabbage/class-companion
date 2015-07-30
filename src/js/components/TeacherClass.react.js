5f27e94837de7b02dd07ada55665b25f71f58ce0
David Hom <dlhom3@gmail.com>
2015-07-29 17:31:19 -0700

    (feat) add delete confirmation to TeacherClass

--- src/js/components/TeacherClass.react.js
+++ src/js/components/TeacherClass.react.js
@@ -1,9 +1,21 @@
 var React = require('react');
 var TeacherActions = require('../actions/TeacherActions');
+var DeleteConfirm = require('./DeleteConfirm.react');
+var Modal = require('react-modal');
 
 var TeacherClass = React.createClass({
   getInitialState: function(){
-    return null;
+    return {
+      deleteConfirmModal: false
+    };
+  },
+
+  openDeteleConfirmModal: function(){
+    this.setState({deleteConfirmModal: true});
+  },
+
+  closeDeleteConfirmModal: function(){
+    this.setState({deleteConfirmModal: false});
   },
 
   removeClass: function(e){
@@ -15,7 +27,7 @@ var TeacherClass = React.createClass({
     return (
       <div className="teacherClass col-md-3">
         <div className="well">
-          <button type="button" className="close" aria-label="Close" onClick={this.removeClass}><span aria-hidden="true">&times;</span></button>
+          <button type="button" className="close" aria-label="Close" onClick={this.openDeteleConfirmModal}><span aria-hidden="true">&times;</span></button>
           <a href={url}>
             <div>
               <img className="classImg" src="./assets/classIcon.png" alt="" />
@@ -23,6 +35,12 @@ var TeacherClass = React.createClass({
             {this.props.classTitle}
           </a>
         </div>
+        <Modal className="deleteConfirmModal"
+          isOpen={this.state.deleteConfirmModal}
+          onRequestClose={this.closeDeleteConfirmModal}>
+          <DeleteConfirm closeDeleteConfirmModal={this.closeDeleteConfirmModal}
+            confirmDelete={this.removeClass}/>
+        </Modal>
       </div>
     );
   }
