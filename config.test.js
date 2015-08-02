720f0c15c99405e8c75c81b442926d4147cdfca0
David Hom <dlhom3@gmail.com>
2015-08-01 23:08:30 -0700

    (cleanup) cleanup unnecessary config file and add new line to eof

--- config.test.js
+++
@@ -1,8 +0,0 @@
-/* these options are for travis-ci
-production options are to be added to config.js*/
-module.exports = {
-  firebaseURI: 'https://cctest.firebaseio.com/',
-  firebaseAdminLogin: 'test@test.com',
-  firebaseAdminPassword: 'test',
-  firebaseAdminUid: 'simplelogin:1'
-};
