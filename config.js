805f93551cb4fe63b0f9ea56a266837d83cf260d
David Hom <dlhom3@gmail.com>
2015-08-03 08:58:06 -0700

    (feat) add server to use firebase secret instead of email/password auth

--- config.js
+++ config.js
@@ -2,7 +2,5 @@
 production options are to be added to config.js*/
 module.exports = {
   firebaseURI: process.env.FIREBASE_URI || 'https://cctest.firebaseio.com/',
-  firebaseAdminLogin: process.env.FIREBASE_ADMIN || 'test@test.com',
-  firebaseAdminPassword: process.env.FIREBASE_PW || 'test',
-  firebaseAdminUid: process.env.FIREBASE_UID || 'simplelogin:1'
+  firebaseSECRET: process.env.FIREBASE_SECRET || 'ZCAC6cOKHAGTQHNC0D8v0Pt9aTI5isILJQt47jm3'
 };
--- server/config/firebaseRef.js
+++ server/config/firebaseRef.js
@@ -9,34 +9,14 @@ var Q = require('q');
 var firebaseURI = config.firebaseURI;
 var firebaseRef = new Firebase(firebaseURI)
 
-var admin = {
-  email: config.firebaseAdminLogin,
-  password: config.firebaseAdminPassword
-};
 
-var authWithPassword = function(userObj) {
-  var deferred = Q.defer();
-
-  firebaseRef.authWithPassword(userObj, function onAuth(err, user) {
-    if (err) {
-      deferred.reject(err);
-    }
-    if (user) {
-      deferred.resolve(user);
-    }
-  });
-
-  return deferred.promise;
-};
-
-authWithPassword(admin)
-  .then(function(authData){
-    console.log('Server logged into Firebase as', authData.uid);
-  })
-  .catch(function(err){
-    console.log(err);
-  });
-
+firebaseRef.authWithCustomToken(config.firebaseSECRET, function(error, authData) {
+  if(!error) {
+    console.log(authData);
+  } else {
+    console.log(error);
+  }
+});
 
 module.exports = {
   firebaseRef: firebaseRef
