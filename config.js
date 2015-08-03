308311070c2288975aade0064f9c3af1bf601792
David Hom <dlhom3@gmail.com>
2015-08-03 10:22:26 -0700

    (fix) remove old config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-/* these options are for travis-ci
-production options are to be added to config.js*/
-module.exports = {
-  firebaseURI: process.env.FIREBASE_URI || 'https://cctest.firebaseio.com/',
-  firebaseSECRET: process.env.FIREBASE_SECRET || 'ZCAC6cOKHAGTQHNC0D8v0Pt9aTI5isILJQt47jm3'
-};
